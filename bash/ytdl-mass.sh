be7f2e56f399721b2507216b1195c1048264729f
Devin <Devin@Devins-MacBook-Pro.local>
2016-09-16 02:11:52 -0400

    Genericized ytdl shell script

--- bash/ytdl-mass.sh
+++ bash/ytdl-mass.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-bk=/mnt/Nepgear/Backup/yt
-bk_dir=$bk/"$1"
+bk="$1"
+bk_dir=$bk/"$2"
 if [ ! -d "$bk_dir" ]
 then
 mkdir "$bk_dir"
 else
-echo -e "$1 already exists! \n Directory not created!"
+echo -e "$2 already exists!\nDirectory not created!"
 fi
 
-a_text="$1-Archive.txt"
-url="$2"
+a_text="$2-Archive.txt"
+url="$3"
 
-youtube-dl -i -f bestvideo+bestaudio --write-description --write-info-json --write-annotations --write-thumbnail --download-archive "$bk_dir/$a_text" --write-sub --convert-subs ass --ffmpeg-location "/home/asuna/bin" -o "$bk_dir/%(title)s.%(ext)s" "$url"
+youtube-dl -i -f bestvideo+bestaudio --write-description --write-info-json --write-annotations --write-thumbnail --download-archive "$bk_dir/$a_text" --write-sub --convert-subs ass --ffmpeg-location "${HOME}/bin" -o "$bk_dir/%(title)s.%(ext)s" "$url"
